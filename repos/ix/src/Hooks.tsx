5606c74bdbbbb37b2aaa30f41fa99aae3c2ee272
Garth Pickell <gpick0@gmail.com>
2020-03-29 18:30:09 -0700

    *** refactors

--- repos/ix/src/Hooks.tsx
+++ repos/ix/src/Hooks.tsx
@@ -1,11 +1,10 @@
 import rx from "@rflect/rx";
 import React, { useRef, useEffect, useState } from "react";
-import { VisualProps } from "./defs";
 
 class JournalerRef {
     journal?: rx.JournalEntry[];
     marker?: number[];
-    set?: (gen: symbol) => void
+    set?: (gen: symbol) => void;
 
     constructor(set: (gen: symbol) => void) {
         this.observe = this.observe.bind(this);
@@ -38,7 +37,7 @@ class JournalerRef {
                 const [journal, topics] = event.record(this.marker);
                 this.journal = journal;
                 this.marker = undefined;    
-                
+
                 if (this.replay(event.gen)) {
                     rx.focus(this.observe, ...topics);
                 }
@@ -58,8 +57,8 @@ class JournalerRef {
     }
 }
 
-export function journal<T extends React.FC<any>>(fc: T): T {
-    function Render(...args: any) {
+export function journal<T extends React.FC<any>>(fc: T) {
+    function render(...args: any) {
         const [gen, set] = useState(Symbol());
         const journaler = useInstance(JournalerRef, set);
         const { effect } = journaler;
@@ -69,7 +68,6 @@ export function journal<T extends React.FC<any>>(fc: T): T {
         rx.notify(journaler);
 
         const marker = journaler.marker = rx.mark();
-
         try {
             return fc.apply(this, args) as React.ReactElement | null;
         } finally {
@@ -77,48 +75,118 @@ export function journal<T extends React.FC<any>>(fc: T): T {
         }
     }
 
-    const Memo = React.memo(Render);
-    return function (props: any) {
-        props = useFantoms(props);
-        return <Memo {...props} />;
-    } as any;
+    return memo(render as any as T);
 }
 
-export function memo<T extends React.FC<any>>(fc: T): T {
-    const Memo = React.memo(fc);
-    return function (props: any) {
-        props = useFantoms(props);
-        return <Memo {...props} />;
-    } as any;
-}
+export class MemoRef {
+    props: any;
+    jsx?: React.ReactElement | null;
 
-export function useFantoms<P>(props: P): P {
-    const ref = useRef<Map<PropertyKey, [Function, Function]>>();
-    if (ref.current === undefined) {
-        ref.current = new Map<PropertyKey, [Function, Function]>();
+    atoms = {} as {
+        [name: string]: undefined | {
+            atom: rx.AnyFunction;
+            func: rx.AnyFunction;
+        };
+    };
+
+    atom(key: string, func: rx.AnyFunction) {
+        let thunk = this.atoms[key];
+        if (thunk === undefined) {
+            thunk = this.atoms[key] = {
+                atom(...args: any) {
+                    const { func } = thunk!;
+                    return func.apply(this, args);
+                },
+                func
+            };
+        } else {
+            thunk.func = func;
+        }
+
+        return thunk.atom;
     }
 
-    const map = ref.current;
-    const result = {} as any;
-    for (const [key, value] of Object.entries(props)) {
-        if (typeof value === "function") {
-            let wrapper = map.get(key);
-            if (wrapper === undefined) {
-                wrapper = [function (...args: any) {
-                    return wrapper![1].apply(this, args);
-                }, value];
-
-                map.set(key, wrapper);
+    compare(props: any) {
+        function check(x: any, y: any) {
+            if (typeof x !== typeof y) {
+                return true;
             }
 
-            wrapper[1] = value;
-            result[key] = wrapper[0];
-        } else {
-            result[key] = value;
+            if (typeof x === "function") {
+                return false;
+            }
+
+            if (Object.is(x, y)) {
+                return false;
+            }
+
+            return true;
         }
+
+        for (const key in props) {
+            if (check(props[key], this.props[key])) {
+                return true;
+            }
+        }
+
+        for (const key in this.props) {
+            if (check(props[key], this.props[key])) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    return result;
+    atomify(props: any) {
+        const result = {} as any;
+        for (const key in props) {
+            const value = props[key];
+            if (typeof value === "function") {
+                result[key] = this.atom(key, value);
+            } else {
+                result[key] = value;
+            }
+        }
+
+        return result;
+    }
+
+    render(Visual: React.FC<any>, props: any) {
+        if (this.jsx === undefined || this.compare(props)) {
+            this.props = props = this.atomify(props);
+            return this.jsx = <Visual {...props} />;
+        }
+
+        return this.jsx;
+    }
+}
+
+export function memo<T extends React.FC<any>>(fc: T) {
+    return function (props: any) {
+        const mr = useInstance(MemoRef);
+        return mr.render(fc, props);
+    } as any as T;
+}
+
+class AtomRef<T, R> {
+    current: T;
+    result: R;
+
+    constructor(current: T, atomizer: Atomizer<T, R>) {
+        this.current = current;
+        this.result = atomizer(this);
+    }
+}
+
+export interface Atomizer<T, R> {
+    (ref: { current: T }): R;
+}
+
+export function useAtom<T, R>(value: T, atomizer: Atomizer<T, R>): R {
+    const atom = useConstant(() => new AtomRef(value, atomizer));
+    atom.current = value;
+    return atom.result;
 }
 
 export function useConstant<T>(f: () => T) {
@@ -160,13 +228,8 @@ class ObserverRef {
 }
 
 export function useObserver(observer: rx.Observer) {
-    const ref = useRef<ObserverRef>();
-    let { current } = ref;
-    if (current !== undefined) {
-        current.observer = observer;
-    } else {
-        current = ref.current = new ObserverRef(observer);
-    }
+    const current = useInstance(ObserverRef, observer);
+    current.observer = observer;
 
     const { forwarder, effect } = current;
     useEffect(effect, [effect]);
@@ -186,8 +249,6 @@ export function useTopics(...topics: unknown[]) {
 }
 
 export function useTrap<T, K extends rx.Func<T>>(topic: T, key: K, f: (event: rx.TrapEvent<T, K, T[K]>, ...args: Parameters<T[K]>) => any) {
-    rx.notify(topic, key);
-
     const observer = useObserver(function (event) {
         if (event.isTrap(topic, key)) {
             f(event, ...event.args);
@@ -195,38 +256,23 @@ export function useTrap<T, K extends rx.Func<T>>(topic: T, key: K, f: (event: rx
     });
 
     rx.focus(observer, topic);
+    rx.notify(topic, key);
 }
 
 export function useJournal<T extends React.FC<any>>(fc: T) {
-    const ref = useRef<T>(fc);
-    ref.current = fc;
-
-    const wrapper = useRef<T>();
-    const { current } = wrapper;
-    if (current === undefined) {
-        const thunk = function (...args: any) {
-            return ref.current.apply(this, args) as any;
-        };
-
-        return wrapper.current = journal(thunk as any as T);
-    }
-
-    return current;
+    return useAtom(fc, function (ref) {
+        return journal(function (...args: any) {
+            const { current } = ref;
+            return current.apply(this, args);
+        }) as any as T;
+    });
 }
 
 export function useMemo<T extends React.FC<any>>(fc: T) {
-    const ref = useRef<T>(fc);
-    ref.current = fc;
-
-    const wrapper = useRef<T>();
-    const { current } = wrapper;
-    if (current === undefined) {
-        const thunk = function (...args: any) {
-            return ref.current.apply(this, args) as any;
-        };
-
-        return wrapper.current = memo(thunk as any as T);
-    }
-
-    return current;
+    return useAtom(fc, function (ref) {
+        return memo(function (...args: any) {
+            const { current } = ref;
+            return current.apply(this, args);
+        }) as any as T;
+    });
 }
